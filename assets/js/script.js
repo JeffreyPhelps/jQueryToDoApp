092264c7ca86fb048d70e18ef36db6f858558467
Jeffrey <phelpsjeffrey@yahoo.com>
2018-10-11 16:02:25 -0600

    App functionality rough draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 // Jeffrey Phelps 2018 jQuery-based ToDo List App
 
-$("li").click(function(){
-    alert("Clicked!")
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(){
+    $(this).parent().fadeOut(1000, function(){
+        $(this).remove();
+    });
+})
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        let todoText = $(this).val();
+        $("ul").append("<li><span>X </span>" + todoText + "</li>")
+        $(this).val("");
+    };
 });
 
